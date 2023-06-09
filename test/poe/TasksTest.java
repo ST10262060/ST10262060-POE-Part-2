6801f4a29e324940fa6b9c7169518228d1c7dc74
ST10262060 <ST10262060@vcconnect.edu.za>
2023-06-09 18:49:41 +0200

    First commit

---
+++ test/poe/TasksTest.java
@@ -0,0 +1,99 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit4TestClass.java to edit this template
+ */
+package poe;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author lab_services_student
+ */
+public class TasksTest {
+    
+    public TasksTest() {
+    }
+
+    @Test
+    public void testCheckTaskDescription() {
+    }
+
+    @Test
+    public void testSetTaskName() {
+    }
+
+    @Test
+    public void testGetTaskName() {
+    }
+
+    @Test
+    public void testSetTaskStatus() {
+    }
+
+    @Test
+    public void testGetTaskStatus() {
+    }
+
+    @Test
+    public void testSetFirstName() {
+    }
+
+    @Test
+    public void testGetFirstName() {
+    }
+
+    @Test
+    public void testSetLastName() {
+    }
+
+    @Test
+    public void testGetLastName() {
+    }
+
+    @Test
+    public void testSetDeveloperDetails() {
+    }
+
+    @Test
+    public void testGetDeveloperDetails() {
+    }
+
+    @Test
+    public void testSetTaskDescription() {
+    }
+
+    @Test
+    public void testGetTaskDescription() {
+    }
+
+    @Test
+    public void testSetTaskDuration() {
+    }
+
+    @Test
+    public void testGetTaskDuration() {
+    }
+
+    @Test
+    public void testSetNumberOfTasks() {
+    }
+
+    @Test
+    public void testGetNumberOfTasks() {
+    }
+
+    @Test
+    public void testCreateTaskID() {
+    }
+
+    @Test
+    public void testPrintTaskDetails() {
+    }
+
+    @Test
+    public void testReturnTotalHours() {
+    }
+    
+}
